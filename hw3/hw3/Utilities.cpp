737c86816cd04e067bb372a360f7f6c84ba4c312
RyanCori <ryancori1@gmail.com>
2016-01-24 16:16:33 -0800

    Recursive attempt
    I have implemented the beginning of recursive reflections based on
    specularity. Needs testing and fixing for sure, but the basic
    implementation is there.

--- hw3/hw3/Utilities.cpp
+++ hw3/hw3/Utilities.cpp
@@ -50,6 +50,10 @@ void Utilities::FindColor(HitInfo info, float* colorVals, int recDepth){
 	colorVals[1] = 0.0;
 	colorVals[2] = 0.0;
 
+	if (recDepth > 5){
+		return;
+	}
+
 	//If the collision actually occured
 	if (info.t < 100){
 
@@ -118,9 +122,9 @@ void Utilities::FindColor(HitInfo info, float* colorVals, int recDepth){
 				term2[2] *= std::fmax(glm::dot(normal, direction), 0);
 				glm::vec3 term3 = (info.collisionObject->specular);
 
-				term3[0] *= pow(std::fmax(glm::dot(normal, half), 0), 2);
-				term3[1] *= pow(std::fmax(glm::dot(normal, half), 0), 2);
-				term3[2] *= pow(std::fmax(glm::dot(normal, half), 0), 2);
+				term3[0] *= pow(std::fmax(glm::dot(normal, half), 0), specular[0]);
+				term3[1] *= pow(std::fmax(glm::dot(normal, half), 0), specular[1]);
+				term3[2] *= pow(std::fmax(glm::dot(normal, half), 0), specular[2]);
 
 				glm::vec3 combinedTerms = term2 + term3;
 				newColor[0] = combinedTerms[0] * term1R;
@@ -128,9 +132,36 @@ void Utilities::FindColor(HitInfo info, float* colorVals, int recDepth){
 				newColor[2] = combinedTerms[2] * term1B;
 				
 
-				colorVals[0] += newColor[0];
-				colorVals[1] += newColor[1];
-				colorVals[2] += newColor[2];
+				//Do reflection if necessary
+				glm::vec3 reflectedRayDirection = 2 * (glm::dot(info.normal, info.raycast.direction))*(info.normal - info.raycast.direction);
+				glm::vec3 reflectedRayOrigin = info.position;
+
+				Ray reflectedRay = Ray();
+				reflectedRay.direction = reflectedRayDirection;
+				reflectedRay.origin = reflectedRayOrigin;
+
+				//Intersect with this reflected ray.
+				HitInfo reflectionInfo = RayIntersect(reflectedRay);
+
+				float* reflectionColor = new float[3];
+				reflectionColor[0] = 0;
+				reflectionColor[1] = 0;
+				reflectionColor[2] = 0;
+
+				if (info.collisionObject->specular[0] != 0 ||
+					info.collisionObject->specular[1] != 0||
+					info.collisionObject->specular[2] != 0){
+					//Make a reflection color and increment the reflection index
+					float* reflectionColor = new float[3];
+					recDepth++;
+					//recursive call to find color
+					FindColor(reflectionInfo, reflectionColor, recDepth);
+				}
+
+
+				colorVals[0] += newColor[0] + info.collisionObject->shininess * reflectionColor[0];
+				colorVals[1] += newColor[1] + info.collisionObject->shininess * reflectionColor[1];
+				colorVals[2] += newColor[2] + info.collisionObject->shininess * reflectionColor[2];
 			}
 		}
 	}
